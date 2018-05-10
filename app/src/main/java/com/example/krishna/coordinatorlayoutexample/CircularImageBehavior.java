c4182f688918de268d482b8aadc054edab1f83e0
Krishna Mandal <krishna.m@encoders.co.in>
2018-05-10 18:52:57 +0530

    init comit

---
+++ app/src/main/java/com/example/krishna/coordinatorlayoutexample/CircularImageBehavior.java
@@ -0,0 +1,8 @@
+package com.example.krishna.coordinatorlayoutexample;
+
+import android.support.design.widget.CoordinatorLayout;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class CircularImageBehavior extends CoordinatorLayout.Behavior<CircleImageView> {
+}
